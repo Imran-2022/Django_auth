c891153e120558d76efe41ef6bb253e6553d2622
Imran-2022 <mdimranulhaque202@gmail.com>
2024-03-13 00:21:40 +0600

    updt

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.home),
+    path('signup/', views.sign_up,name='sign_up'),
+    path('login/', views.user_login,name='login'),
+    path('', views.home,name='home'),
+    path('profile/', views.profile,name='profile'),
+
 ]
--- firstapp/views.py
+++ firstapp/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import RegisterForm
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate,login,logout
 # Create your views here.
 
 def home(request):
+    return render(request,'home.html')
+
+
+def sign_up(request):
     if request.method=="POST":
         form=RegisterForm(request.POST)
         if form.is_valid():
@@ -15,4 +21,28 @@ def home(request):
             # print(form.cleaned_data)
     else:
         form=RegisterForm()
-    return render(request, 'home.html',{'form':form})
+    return render(request, 'signup.html',{'form':form})
+
+
+def user_login(request):
+    if request.method=="POST":
+        form=AuthenticationForm(request=request,data=request.POST)
+        if form.is_valid():
+            name=form.cleaned_data['username']
+            userpass=form.cleaned_data['password']
+            user=authenticate(username=name,password=userpass)
+            # chk krtechi user database a ache ki na ! 
+            
+            if user is not None:
+                login(request,user)
+                return redirect('profile')
+    else:
+        form =AuthenticationForm()
+    return render(request,'./login.html',{'form':form})
+
+
+def profile(request):
+    return render(request,'profile.html',{'user':request.user})
+
+
+
